9323c14543344daf25b187a8b76abdc3897fb466
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-08-08 09:39:34 +0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
 import { Inter } from "next/font/google";
-import { Provider } from "react-redux";
-import { store } from "@/redux/store/store";
 import BgAnimation from "@/components/bg-animation";
 
 import "slick-carousel/slick/slick.css";
@@ -19,7 +17,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head></head>
-      {/* <Provider store={store}> */}
         <body className={inter.className}>
           <BgAnimation />
           {children}
--- redux/store/store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import authReducer, { name as authName } from "@/redux/slice/authSlice";
-
-// export const store = configureStore({
-//   reducer: {
-//     [authName]: authReducer,
-//   },
-// });
